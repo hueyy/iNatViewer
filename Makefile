f3e48e8daebdb2e78d504641d4f449982a379ebf
Huey <hello@huey.xyz>
2024-11-23 19:55:47 +0800

    feature: add routing

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 build: $(shell find src) public/* node_modules
 	mkdir -p build
-	pnpm run shadow-cljs release app
+	pnpm exec shadow-cljs release app
 	rsync -aLz --exclude js --exclude '.*.swp' public/ build
 	touch build
 
@@ -19,7 +19,7 @@ watch:
 	make -j2 watch-shadow-cljs watch-tailwindcss
 
 repl: node_modules
-	pnpm run shadow-cljs cljs-repl app
+	pnpm exec shadow-cljs cljs-repl app
 
 clean:
 	rm -rf build
--- src/inatviewer/core.cljs
+++ src/inatviewer/core.cljs
@@ -1,19 +1,55 @@
 (ns inatviewer.core
   (:require
    [reagent.core :as r]
-   [reagent.dom :as rdom]))
+   [reagent.dom :as rdom]
+   [reitit.frontend :as rf]
+   [reitit.frontend.easy :as rfe]
+   [reitit.coercion.spec :as rss]
+   [spec-tools.data-spec :as ds]
+   [clojure.spec.alpha :as s]
+   [fipp.edn :as fedn]))
 
-(defonce state (r/atom {}))
+(defonce match (r/atom nil))
 
-(defn component-main [_state]
+(defn home-page [_state]
   [:div
-   [:h1 "NAME"]
-   [:p "Welcome to the app!"]
-   [:button {:on-click #(js/alert "Hello world!")} "click me"]])
+   [:h1 "iNatViewer"]
+   [:form {:action (rfe/href ::view) :method "GET"}
+    [:input {:name "url" :placeholder "URL" :value "https://www.inaturalist.org/observations?q=huey.xyz&user_id=hueyl"}]
+    [:button {:type "submit"} "VIEW"]]])
+
+(defn viewer [match]
+  (let [{:keys [query]} (:parameters match)
+        url (:url query)]
+    [:div
+     [:h1 "VIEWER"]
+     [:p url]]))
+
+(defn current-page []
+  [:div
+   [:ul
+    [:li [:a {:href (rfe/href ::frontpage)} "Homepage"]]
+    [:li [:a {:href (rfe/href ::view)} "Viewer"]]]
+   (if @match
+     (let [view (:view (:data @match))]
+       [view @match])
+     [:pre (with-out-str (fedn/pprint @match))])])
+
+(def routes
+  [["/"
+    {:name ::frontpage
+     :view home-page}]
+   ["/view"
+    {:name ::view
+     :view viewer
+     :parameters {:query (s/keys :req-un [::url])}}]])
 
 (defn start {:dev/after-load true} []
-  (rdom/render [component-main state]
-               (js/document.getElementById "app")))
+  (rfe/start!
+   (rf/router routes {:data {:coercion rss/coercion}})
+   (fn [m] (reset! match m))
+   {:use-fragment false})
+  (rdom/render [current-page] (js/document.getElementById "app")))
 
 (defn init []
   (start))
